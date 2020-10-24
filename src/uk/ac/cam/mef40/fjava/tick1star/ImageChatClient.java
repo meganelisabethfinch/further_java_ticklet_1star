040b0ba4de1474aed3640f555c60096bb581e3fb
Megan Elisabeth Finch <meganelisabethfinch@gmail.com>
2020-10-24 22:45:18 +0100

    Implemented ImageChatClient

--- src/uk/ac/cam/mef40/fjava/tick1star/ImageChatClient.java
+++ src/uk/ac/cam/mef40/fjava/tick1star/ImageChatClient.java
@@ -16,64 +16,151 @@
 
 package uk.ac.cam.mef40.fjava.tick1star;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
 public class ImageChatClient extends JFrame {
     public static void main(String[] args) {
-        String server = "example";
+        String server = null;
         int port = 0;
+
+        // Validate input
+
+        try {
+            server = args[0];
+            port = Integer.parseInt(args[1]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            System.err.println("This application requires two arguments: <machine> <port>");
+            return;
+        }
 
         var client = new ImageChatClient(server, port);
     }
 
     public ImageChatClient(String server, int port) {
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
-        // create a swing gui with an AWT canvas and an AWT button labelled 'Upload'
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Image Chat");
 
-        // SET UP CANVAS
-        var canvasPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        final Canvas canvas = new Canvas();
-        canvas.setSize(400, 400);
-        canvas.setBackground(new Color(255,255,255));
-        canvasPanel.add(canvas);
-
-        // SET UP UPLOAD BUTTON
-        var uploadPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        var button = new Button("Upload");
-        button.addActionListener(e -> {
-            JFileChooser chooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG images", "jpg", "jpeg");
-            chooser.setFileFilter(filter);
-            int img = chooser.showOpenDialog(this.getParent());
-            if (img == JFileChooser.APPROVE_OPTION) {
-                System.out.println("Uploaded " + chooser.getSelectedFile().getName());
-                // canvas.getGraphics().drawBytes();
-            }
-        });
-        uploadPanel.add(button);
-
-        // LAYOUT CANVAS AND BUTTON
-        JPanel borderPanel = new JPanel(new BorderLayout());
-        borderPanel.add(canvasPanel, BorderLayout.NORTH);
-        borderPanel.add(uploadPanel, BorderLayout.SOUTH);
-
-        JPanel container = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        container.add(borderPanel);
-        this.getContentPane().add(container);
-
-        this.pack();
-        this.setResizable(false);
-        this.setVisible(true);
-
         // use one thread to listen on a socket object connected to server and
         // draw any image sent by the server to the canvas object
-        // Thread output =
+        try {
+            final Socket s = new Socket(server, port);
+
+            // SET UP CANVAS
+            var canvasPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            final Canvas canvas = new Canvas();
+            canvas.setSize(400, 400);
+            canvas.setBackground(new Color(255,255,255));
+            canvasPanel.add(canvas);
+
+            // SET UP UPLOAD BUTTON
+            var uploadPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            var button = new Button("Upload");
+            button.addActionListener(e -> {
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("JPEG images", "jpg", "jpeg");
+                chooser.setFileFilter(filter);
+                int uploaded = chooser.showOpenDialog(this.getParent());
+                if (uploaded == JFileChooser.APPROVE_OPTION) {
+                    // replace with send to server
+                    System.out.println("Uploaded " + chooser.getSelectedFile().getName());
+                    try {
+                        RenderedImage img = ImageIO.read(chooser.getSelectedFile());
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        ImageIO.write(img, "jpg", baos);
+                        s.getOutputStream().write(baos.toByteArray());
+                        // canvas.getGraphics().drawImage(img, 0,0, null);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            uploadPanel.add(button);
+
+            // LAYOUT CANVAS AND BUTTON
+            JPanel borderPanel = new JPanel(new BorderLayout());
+            borderPanel.add(canvasPanel, BorderLayout.NORTH);
+            borderPanel.add(uploadPanel, BorderLayout.SOUTH);
+
+            JPanel container = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            container.add(borderPanel);
+            this.getContentPane().add(container);
+
+            this.pack();
+            this.setResizable(false);
+            this.setVisible(true);
+
+            Thread output = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        // Listen for images sent by the server
+                        byte[] buffer = new byte[1024];
+                        InputStream is = s.getInputStream();
+
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        // Read the data from the socket into a small buffer
+                        // Perform some manual checks to see if end of JPEG reached
+                        // Then write data out to a second stream to feed into ImageIO.read(InputStream is).
+
+                        byte prev = 0; // Initialise to anything not -1
+                        boolean eof = false; // true if -1, 39 seen
+                        int imgAt = 0;
+
+                        while (true) {
+                            int bytesRead = is.read(buffer);
+                            // check whether two consecutive bytes have -1, 39
+
+                            // Draw image
+                            if (bytesRead > 0) {
+                                for (int i = 0; !eof && i < bytesRead; i++) {
+                                    if (prev == -1 && buffer[i] == -39) {
+                                        eof = true; // image found
+                                        imgAt = i + 1;
+                                    }
+
+                                    prev = buffer[i];
+                                }
+
+                                if (eof) {
+                                    baos.write(buffer, 0, imgAt);
+                                    ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+                                    Image img = ImageIO.read(bais);
+                                    canvas.getGraphics().drawImage(img, 0,0, null);
+                                    baos.reset();
+                                    // baos.write(buffer, imgAt, bytesRead - imgAt);
+                                } else {
+                                    baos.write(buffer, 0, bytesRead);
+                                }
+                            }
+
+                            eof = false;
+                        }
+
+                    } catch (IOException e) {
+                        System.err.println("Could not read data from server.");
+                        return;
+                    }
+                }
+            };
+            output.setDaemon(true);
+            output.start();
+        } catch (IOException e) {
+            System.err.format("Cannot connect to %s on port %d\n", server, port);
+        }
+
 
         // use other thread to listen for user input
     }
